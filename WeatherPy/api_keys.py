38642e833482d6277aa4761d17de109b761b79d6
audupa1 <>
2023-02-04 11:20:54 -0600

    keys added

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "898e4dd31c8311399168a32f6b7fd1a2"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "38004fe101234c5b8f4daff3c1ccf22c"
